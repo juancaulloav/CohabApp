95f9f516a09f87810d26695ba062c7869105abb9
juancaulloav <juancarlosulloav@gmail.com>
2022-10-12 21:47:45 -0300

    Agregado NoteViews

--- src/Cohab/Pages/CohabPage.jsx
+++ src/Cohab/Pages/CohabPage.jsx
@@ -1,18 +1,20 @@
-import { Typography } from "@mui/material";
+
 import { CohabLayout } from "../layout/CohabLayout";
+import { NoteViews } from "../views/NoteViews";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const CohabPage = () => {
   return (
     <CohabLayout>
-      <Typography>
+      {/* <Typography>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
         corrupti magni debitis? Laudantium id deleniti officia, temporibus modi
         beatae nulla voluptatum debitis necessitatibus amet, ipsum eius
         assumenda adipisci saepe delectus!
-      </Typography>
-      {/**NothingSelected */}
-
-      {/**NoteView */}
+      </Typography> */}
+      <NoteViews />
+      {/* <NothingSelectedView /> */}
+  
     </CohabLayout>
   );
 };
