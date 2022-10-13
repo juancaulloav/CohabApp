588d7d5c154d9a9e5c8e014e5edceb7ad7499b74
juancaulloav <juancarlosulloav@gmail.com>
2022-10-13 00:07:05 -0300

    Frontend 1.0

--- src/Cohab/Pages/CohabPage.jsx
+++ src/Cohab/Pages/CohabPage.jsx
@@ -1,4 +1,5 @@
-
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { CohabLayout } from "../layout/CohabLayout";
 import { NoteViews } from "../views/NoteViews";
 import { NothingSelectedView } from "../views/NothingSelectedView";
@@ -14,7 +15,20 @@ export const CohabPage = () => {
       </Typography> */}
       <NoteViews />
       {/* <NothingSelectedView /> */}
-  
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize :30}}/>
+      </IconButton>
     </CohabLayout>
   );
 };
